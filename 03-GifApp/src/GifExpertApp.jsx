d17269d0c75ff62f20bbfa5f5f24012e48b4629a
emmanuelanguiano12 <emmanuelanguiano1223@gmail.com>
2024-06-27 14:02:50 -0600

    Gif App

---
+++ 03-GifApp/src/GifExpertApp.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import { AddCategory, GifGrid } from "./components/index";
+
+export const GifExpertApp = () => {
+
+  const [ categories, setCategories ] = useState(['cr7']);
+
+  const onAddCategory = (newCategory) => {
+    if(categories.includes(newCategory) ) return
+
+    setCategories([ newCategory, ...categories ])
+  }
+
+  return (
+    <>
+        <h1>GifExpertApp</h1>
+        <AddCategory onNewCategory={(event) => onAddCategory(event)} />
+
+        {
+          categories.map(category => (
+              <GifGrid key={category} category={category} />
+            )
+        )}
+    </>
+  )
+}
